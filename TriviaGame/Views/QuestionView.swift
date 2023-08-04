5aee1d7662ac676b1c18141d8c18655d1be98a06
Elaidzha1940 <eli080293@gmail.com>
2023-08-04 15:12:13 +0300

    added QuestionView

---
+++ TriviaGame/Views/QuestionView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: TriviaGame
+//  File: QuestionView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 04.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
