34c8fd2bd6975d483393561255b4c7a730e2bbd7
Elaidzha1940 <eli080293@gmail.com>
2023-08-04 14:24:47 +0300

    TriviaGameApp

---
+++ TriviaGame/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: TriviaGame
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 04.08.2023
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Elaidzha Shchukin on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
