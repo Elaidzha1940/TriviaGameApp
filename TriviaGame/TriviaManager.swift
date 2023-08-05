c7b3d809353008cb5005afa3bf0f64dedc100d85
Elaidzha1940 <eli080293@gmail.com>
2023-08-05 09:18:04 +0300

    added TriviaManager

---
+++ TriviaGame/TriviaManager.swift
@@ -0,0 +1,16 @@
+//  /*
+//
+//  Project: TriviaGame
+//  File: TriviaManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
+
+class TriviaManager: ObservableObject {
+    
+}
