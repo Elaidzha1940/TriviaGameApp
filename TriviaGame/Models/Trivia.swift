38ef058ec8fdad609d3b17aac8312af0af43e8b9
Elaidzha1940 <eli080293@gmail.com>
2023-08-05 08:54:44 +0300

    added Trivia

---
+++ TriviaGame/Models/Trivia.swift
@@ -0,0 +1,12 @@
+//  /*
+//
+//  Project: TriviaGame
+//  File: Trivia.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 05.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
