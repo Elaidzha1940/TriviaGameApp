f944781530b4b88f2fdad37cc634ec6eeced5d87
Elaidzha1940 <eli080293@gmail.com>
2023-08-05 09:27:56 +0300

    added TriviaManager

--- TriviaGame/TriviaManager.swift
+++ TriviaGame/TriviaManager.swift
@@ -15,8 +15,17 @@ class TriviaManager: ObservableObject {
     
     func fetchTrivia() async {
         
-        guard let url = URL(string: "https://opentdb.com/api.php?amount=10") else { fatalError("Missing URL") {}
-        
-            let urlRequest = URLRequest 
+        guard let url = URL(string: "https://opentdb.com/api.php?amount=10") else { fatalError("Missing URL") }
+            
+            let urlRequest = URLRequest(url: url)
+            
+            do {
+               let (data, response) = try await URLSession.shared.data(for: urlRequest)
+                
+                guard let (response as? HTTPURLResponse)?.statusCode = 200 else { fatalError("Errror while fetching")}
+                
+            } catch {
+                print("Error fetching trivis: \(error)")
+            }
+        }
     }
-}
